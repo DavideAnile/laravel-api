581f25a77ffeac90d49439f97745a340d937dd53
DavideAnile <davide.anile16@gmail.com>
2023-06-01 08:59:25 +0200

    feat :
    Projects Filter added on index

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,22 +4,48 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index(){
-        $projects = Project::with('type', 'technologies')->get();
+    public function index(Request $request){
 
-        return response()->json([
-            'success' => true,
-            'results' => $projects,
-        ]);
+        $types = Type::all();
+
+        $requestData = $request->all();
+        
+        if($request->has('type_id') && $requestData['type_id']){
+
+            $projects = Project::where('type_id' , $requestData['type_id'])->with('type' , 'technologies')->get();
+
+            return response()->json([
+                'success' => true,
+                'results' => $projects,
+                'types' => $types,
+            ]);
+
+        } else {
+
+            $projects = Project::with('type', 'technologies')->get();
+    
+            
+    
+            return response()->json([
+                'success' => true,
+                'results' => $projects,
+                'types' => $types,
+            ]);
+
+        }
+
     }
 
     public function show($slug){
         
         $project = Project::where('slug', $slug)->with('type' , 'technologies')->first();
+
+        
 
         if($project == null) {
 
@@ -33,7 +59,8 @@ class ProjectController extends Controller
 
             return response()->json([
                 'success' => true,
-                'project' => $project, 
+                'project' => $project,
+                  
             ]);
 
         }
